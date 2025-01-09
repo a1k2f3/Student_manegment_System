210fe4e5653146f798268c4ba7490d364c0bf440
akif <su92-bssem-f22-118@superior.edu.pk>
2025-01-09 06:58:09 +0500

    eoor handling

--- Admin/Controllers/Leave.js
+++ Admin/Controllers/Leave.js
@@ -4,7 +4,6 @@ const Leave = new Schema({
   Email: {type:String,unique:true},
   subject:String,
   message: String ,
-  time:Timestamp,
   date: { type: Date, default: Date.now },
 });
 export const  Leaves = mongoose.model('Leave', Leave);
